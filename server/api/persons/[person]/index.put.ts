0bb54ecb195304dcdb53c5dddcb4580f4c33932d
Hendrik Oenings <caylee@caylee.de>
2025-08-18 20:16:39 +0200

    Add OpenSlides person integration

--- server/api/persons/[person]/index.put.ts
+++ server/api/persons/[person]/index.put.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
 	const database = useDatabase()
+	const client = useOpenslides()
 
 	const params = await getValidatedRouterParams(event, z.object({
 		person: idSchema,
@@ -11,18 +12,34 @@ export default defineEventHandler(async (event) => {
 
 	const body = await readValidatedBody(event, createUpdateSchema(persons).omit({ id: true }).parseAsync)
 
-	const result = await database
-		.update(persons)
-		.set(body)
-		.where(eq(persons.id, params.person))
+	await database.transaction(async (tx) => {
+		const result = await tx.update(persons).set(body).where(eq(persons.id, params.person))
+		if(result.rowCount === 0) {
+			throw createError({
+				statusCode: 404,
+				statusMessage: 'Person nicht gefunden',
+				data: {
+					personId: params.person,
+				},
+			})
+		}
 
-	if(result.rowCount === 0) {
-		throw createError({
-			statusCode: 404,
-			statusMessage: 'Person nicht gefunden',
-			data: {
-				personId: params.person,
-			},
+		await client.connect()
+		const [ [ { id } ] ] = await client.presenters.search_users({
+			permission_type: 'organization',
+			permission_id: 1,
+			search: [ { saml_id: body.email } ],
 		})
-	}
+		await client.user.update({
+			id,
+			saml_id: body.email,
+			username: body.email,
+			first_name: body.callName || body.firstName,
+			last_name: body.lastName,
+			email: body.email,
+			...(body.pronouns ? { pronoun: body.pronouns } : {}),
+			is_physical_person: true,
+			can_change_own_password: false,
+		})
+	})
 })
--- server/api/persons/index.post.ts
+++ server/api/persons/index.post.ts
@@ -2,11 +2,25 @@ import { createInsertSchema } from 'drizzle-zod'
 
 export default defineEventHandler(async (event) => {
 	const database = useDatabase()
+	const client = useOpenslides()
 
 	const body = await readValidatedBody(event, createInsertSchema(persons).omit({ id: true }).parseAsync)
 
-	return await database
-		.insert(persons)
-		.values(body)
-		.returning({ id: persons.id })
+	return await database.transaction(async (tx) => {
+		const [ result ] = await tx.insert(persons).values(body).returning({ id: persons.id })
+
+		await client.connect()
+		await client.user.create({
+			saml_id: body.email,
+			username: body.email,
+			first_name: body.callName || body.firstName,
+			last_name: body.lastName,
+			email: body.email,
+			...(body.pronouns ? { pronoun: body.pronouns } : {}),
+			is_physical_person: true,
+			can_change_own_password: false,
+		})
+
+		return result
+	})
 })
