c37e0dcc340385b63f6128403f0a630d61961375
Hendrik Oenings <caylee@caylee.de>
2025-11-05 12:00:45 +0100

    feat: enhanced API for authentication connectors

--- server/api/persons/index.get.ts
+++ server/api/persons/index.get.ts
@@ -1,9 +1,21 @@
-export default defineEventHandler(async (_event) => {
+import z from 'zod'
+
+export default defineEventHandler(async (event) => {
 	await checkPermission('persons.read')
+
+	const query = await getValidatedQuery(event, async (data) => await z.strictObject({
+		email: z.email().optional(),
+	}).parseAsync(data))
 
 	const database = useDatabase()
 
-	const persons = await database.query.persons.findMany()
+	const persons = await database.query.persons.findMany({
+		...(query.email
+			? {
+				where: (persons, { eq }) => eq(persons.email, query.email ?? ''),
+			}
+			: {}),
+	})
 
 	return persons
 })
---
+++ server/api/persons/index.put.ts
@@ -0,0 +1,52 @@
+import { z } from 'zod'
+
+export default defineEventHandler(async (event) => {
+	await checkPermission('persons.read')
+	await checkPermission('persons.create')
+	await checkPermission('persons.update')
+
+	const query = await getValidatedQuery(event, async (data) => await z.strictObject({
+		email: z.email(),
+	}).parseAsync(data))
+
+	const body = await readValidatedBody(event, async (data) => await z.strictObject({
+		firstName: z.string(),
+		lastName: z.string(),
+		email: z.email(),
+	}).parseAsync(data))
+
+	const database = useDatabase()
+
+	const person = await database.query.persons.findFirst({
+		where: (persons, { eq }) => eq(persons.email, query.email),
+		columns: {
+			id: true,
+			firstName: true,
+			lastName: true,
+			email: true,
+		},
+	})
+
+	if(!person) {
+		await $fetch('/api/persons', {
+			method: 'POST',
+			headers: {
+				cookie: getHeader(event, 'cookie') ?? '',
+			},
+			body,
+		})
+		return
+	}
+
+	if(person.firstName === body.firstName && person.lastName === body.lastName && person.email === body.email) {
+		return
+	}
+
+	await $fetch(`/api/persons/${person.id}`, {
+		method: 'PUT',
+		headers: {
+			cookie: getHeader(event, 'cookie') ?? '',
+		},
+		body,
+	})
+})
